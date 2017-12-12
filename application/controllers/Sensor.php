b1d991a59434c1c1174920473ffb77ab01f7a2f8
Christian Bruno <christianbpn@gmail.com>
2017-12-12 11:09:58 -0300

    Mudança de leitura de GET para POST

--- application/controllers/Sensor.php
+++ application/controllers/Sensor.php
@@ -42,7 +42,7 @@ class Sensor extends REST_Controller
       $this->response($response);
   }
 
-  public function servidor_get() {
+  public function servidor_post() {
     $dados = $this->Sensor_model->get_leitura();
     $this->response($dados);
   }
